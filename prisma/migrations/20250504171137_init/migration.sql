2a2c91103d8948c99f0eb3d911ab825ef263fb14
frnzdev <felipeacfornazeiro@gmail.com>
2025-05-04 15:43:28 -0300

    um commit antes da possivel merda

--- prisma/migrations/20250504171137_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Necessidade" (
-    "id" SERIAL NOT NULL,
-    "nome" TEXT NOT NULL,
-    "item" TEXT NOT NULL,
-    "quantidade" INTEGER NOT NULL,
-
-    CONSTRAINT "Necessidade_pkey" PRIMARY KEY ("id")
-);
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,6 @@ import Help from "./components/Help";
 import ThemeProvider from "./providers/theme-provider";
 import BackToTopButton from "./components/BackUp";
 import { AosInit } from "./components/aos-init";
-import Necessidades from "./components/Necessidades";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -45,7 +44,6 @@ export default function RootLayout({
             <BackToTopButton />
             {children}
             <AosInit />
-            <Necessidades />
             <Footer />
           </ThemeProvider>
         </ClerkProvider>
